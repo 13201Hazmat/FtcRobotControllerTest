a5b05672662e9d196076981fe66ce862eda0d603
louisliu2 <26liul@elmbrookstudents.org>
2022-10-15 11:46:01 -0500

    made test op arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/ArmTestOpMode.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.TestOpModes;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.SubSystems.Arm;
+
+@TeleOp(name = "TestOpArm", group = "TestOp")
+public class ArmTestOpMode extends LinearOpMode {
+
+    public Arm arm;
+
+    public void runOpMode() throws InterruptedException{
+
+        arm = new Arm(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            arm.turnArmBrakeModeOn();
+
+            //Extend the arm based on the right joystick
+            if (gamepad2.left_stick_x > 0) {
+                arm.extendArm();
+                arm.runArmToLevel(gamepad2.left_stick_x);
+            }
+
+            //retract the arm based on the right joystick
+            if (gamepad2.left_stick_x < 0) {
+                arm.retractArm();
+                arm.runArmToLevel(gamepad2.left_stick_x);
+            }
+
+            //Move arm to preset positions
+            if (gamepad2.x){
+                arm.moveToArmLowJunction();
+                arm.runArmToLevel(arm.MED_POWER);
+            }
+
+            if (gamepad2.b){
+                arm.moveToArmHighJunction();
+                arm.runArmToLevel(arm.MED_POWER);
+            }
+
+            if (gamepad2.a){
+                arm.moveToArmGroundJunction();
+                arm.runArmToLevel(arm.MED_POWER);
+            }
+            if (gamepad2.y){
+                arm.moveToArmMidJunction();
+                arm.runArmToLevel(arm.MED_POWER);
+            }
+
+        }
+
+    }
+
+}
