87ef1e9d803ad04cbf51ce4cb910bd385c6ae84a
louisliu2 <26liul@elmbrookstudents.org>
2022-10-15 11:47:02 -0500

    added to arm test op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/ArmTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/ArmTestOpMode.java
@@ -22,32 +22,38 @@ public class ArmTestOpMode extends LinearOpMode {
             if (gamepad2.left_stick_x > 0) {
                 arm.extendArm();
                 arm.runArmToLevel(gamepad2.left_stick_x);
+                arm.runArmToLevelState = false;
             }
 
             //retract the arm based on the right joystick
             if (gamepad2.left_stick_x < 0) {
                 arm.retractArm();
                 arm.runArmToLevel(gamepad2.left_stick_x);
+                arm.runArmToLevelState = false;
             }
 
             //Move arm to preset positions
             if (gamepad2.x){
                 arm.moveToArmLowJunction();
                 arm.runArmToLevel(arm.MED_POWER);
+                arm.runArmToLevelState = false;
             }
 
             if (gamepad2.b){
                 arm.moveToArmHighJunction();
                 arm.runArmToLevel(arm.MED_POWER);
+                arm.runArmToLevelState = false;
             }
 
             if (gamepad2.a){
                 arm.moveToArmGroundJunction();
                 arm.runArmToLevel(arm.MED_POWER);
+                arm.runArmToLevelState = false;
             }
             if (gamepad2.y){
                 arm.moveToArmMidJunction();
                 arm.runArmToLevel(arm.MED_POWER);
+                arm.runArmToLevelState = false;
             }
 
         }
