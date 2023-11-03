d32e2421a302cf3a6f2a8374b6182fd5780937f6
amjadj <amjad.jabs@gmail.com>
2023-11-02 20:18:18 -0500

    Update based on RR update on 2023-10-21

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/TestClimber2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpModes/TestClimber2.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.TestOpModes;
 
 import static com.qualcomm.robotcore.util.ElapsedTime.Resolution.MILLISECONDS;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
