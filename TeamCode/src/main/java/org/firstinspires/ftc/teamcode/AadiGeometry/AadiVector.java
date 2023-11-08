3736d5baad25cf39633db450318db7096d726afc
Aaditya-Tiwari <24aaditf@elmbrookstudents.org>
2023-11-07 20:01:57 -0600

    aadipose

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AadiGeometry/AadiVector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AadiGeometry/AadiVector.java
@@ -1,4 +1,16 @@
 package org.firstinspires.ftc.teamcode.AadiGeometry;
 
+
 public class AadiVector {
+    private double slidesLength;
+    private double handAngle;
+    private double armAngle;
+
+    public AadiVector(double slidesLength, double handAngle, double armAngle){
+        this.slidesLength = slidesLength;
+        this.handAngle = handAngle;
+        this.armAngle = armAngle;
+    }
+
+    public void set
 }
